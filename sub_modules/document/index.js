fcfea24c07531e4620f3e6790bcc3e2043bcfbef
yuhaoxuan <2048254645@qq.com>
2024-03-12 15:47:47 +0800

    12

--- sub_modules/document/index.js
+++
@@ -1,7 +0,0 @@
-var fetch = require('google-docs-fetch');
-
-function get(key, cb) {
-    fetch(key, cb);
-};
-
-exports.get = get;
--- sub_modules/spreadsheet/index.js
+++
@@ -1,6 +0,0 @@
-var sheets = require('google-drive-sheets');
-
-exports.get = function get(key, cb) {
-    var mySheet = new sheets(key);
-    mySheet.getRows(1, cb);		// XXX
-};
